c416253cd2e3fcab5904778f4ab9d2910eeb2a94
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-07-04 21:22:18 -0500

    40. Cambiar el nombre de los chunks - LazyLoad Basico Listo

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -10,9 +10,20 @@ interface Route {
 	name: string;
 }
 
-const Lazy1 = lazy(() => import('../01-lazyload/pages/LazyPage1'));
-const Lazy2 = lazy(() => import('../01-lazyload/pages/LazyPage2'));
-const Lazy3 = lazy(() => import('../01-lazyload/pages/LazyPage3'));
+const Lazy1 = lazy(
+	() =>
+		import(/* webpackChunkName: "LazyPage1" */ '../01-lazyload/pages/LazyPage1')
+);
+
+const Lazy2 = lazy(
+	() =>
+		import(/* webpackChunkName: "LazyPage2" */ '../01-lazyload/pages/LazyPage2')
+);
+
+const Lazy3 = lazy(
+	() =>
+		import(/* webpackChunkName: "LazyPage3" */ '../01-lazyload/pages/LazyPage3')
+);
 
 export const routes: Route[] = [
 	{
