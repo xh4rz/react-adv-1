5f6024925d2f8d58d457b20705d8b725a114e8fc
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-07-20 22:59:55 -0500

    81. Continuación de la sección - Control Props

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -12,6 +12,12 @@ const product = {
 	img: './coffee-mug.png'
 };
 
+const product2 = {
+	id: '2',
+	title: 'Coffee Mug - Meme',
+	img: './coffee-mug2.png'
+};
+
 export const ShoppingPage = () => {
 	return (
 		<div>
@@ -24,18 +30,10 @@ export const ShoppingPage = () => {
 					<ProductCard.Buttons className="custom-buttons" />
 				</ProductCard>
 
-				<ProductCard product={product} className="bg-dark text-white">
+				<ProductCard product={product2} className="bg-dark text-white">
 					<ProductImage className="custom-image" />
 					<ProductTitle className="text-bold" />
 					<ProductButtons className="custom-buttons" />
-				</ProductCard>
-
-				<ProductCard product={product} style={{ backgroundColor: '#70D1F8' }}>
-					<ProductImage
-						style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }}
-					/>
-					<ProductTitle style={{ fontWeight: 'bold' }} />
-					<ProductButtons style={{ display: 'flex', justifyContent: 'end' }} />
 				</ProductCard>
 			</div>
 		</div>
