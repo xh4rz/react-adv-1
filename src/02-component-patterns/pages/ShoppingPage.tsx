90fd917511722ebe09ab212da2ee3f44195d0598
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-07-25 22:59:10 -0500

    87. Mostrar items del carrito de compras

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -74,32 +74,26 @@ export const ShoppingPage = () => {
 			</div>
 
 			<div className="shopping-cart">
-				<ProductCard
-					product={product2}
-					className="bg-dark text-white"
-					style={{ width: '100px' }}
-				>
-					<ProductImage
-						className="custom-image"
-						style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }}
-					/>
-					<ProductButtons className="custom-buttons" />
-				</ProductCard>
-				<ProductCard
-					product={product1}
-					className="bg-dark text-white"
-					style={{ width: '100px' }}
-				>
-					<ProductImage
-						className="custom-image"
-						style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }}
-					/>
-					<ProductButtons className="custom-buttons" />
-				</ProductCard>
-			</div>
-
-			<div>
-				<code>{JSON.stringify(shoppingCart, null, 5)}</code>
+				{Object.entries(shoppingCart).map(([key, product]) => (
+					<ProductCard
+						key={key}
+						product={product}
+						className="bg-dark text-white"
+						style={{ width: '100px' }}
+					>
+						<ProductImage
+							className="custom-image"
+							style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }}
+						/>
+						<ProductButtons
+							className="custom-buttons"
+							style={{
+								display: 'flex',
+								justifyContent: 'center'
+							}}
+						/>
+					</ProductCard>
+				))}
 			</div>
 		</div>
 	);
