874170fa2c0c3a73ff0a9277c2b1bf1766da2b56
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-07-30 23:42:22 -0500

    91. Controlando el estado desde el padre

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -39,15 +39,32 @@ export const ShoppingPage = () => {
 		product: Product;
 	}) => {
 		setShoppingCart((oldShoppingCart) => {
-			if (count === 0) {
-				const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-				return rest;
+			const productInCart: ProductInCart = oldShoppingCart[product.id] || {
+				...product,
+				count: 0
+			};
+
+			if (Math.max(productInCart.count + count, 0) > 0) {
+				productInCart.count += count;
+				return {
+					...oldShoppingCart,
+					[product.id]: productInCart
+				};
 			}
 
-			return {
-				...oldShoppingCart,
-				[product.id]: { ...product, count }
-			};
+			// Borrar el producto
+			const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+			return rest;
+
+			// if (count === 0) {
+			// 	const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+			// 	return rest;
+			// }
+
+			// return {
+			// 	...oldShoppingCart,
+			// 	[product.id]: { ...product, count }
+			// };
 		});
 	};
 
