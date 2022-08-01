56dc6c68d0b9e10727f742c2019068e8ea3b0074
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-07-31 22:27:07 -0500

    97. Continuación de la sección

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -4,60 +4,28 @@ import {
 	ProductTitle,
 	ProductButtons
 } from '../components';
-import { useShoppingCart } from '../hooks/useShoppingCart';
 import { products } from '../data/products';
 import '../styles/custom-styles.css';
 
-export const ShoppingPage = () => {
-	const { shoppingCart, onProductCountChange } = useShoppingCart();
+const product = products[0];
 
+export const ShoppingPage = () => {
 	return (
 		<div>
 			<h1>Shopping Store</h1>
 			<hr />
-			<div style={{ display: 'flex', flexDirection: 'row', flexWrap: 'wrap' }}>
-				{products.map((product) => (
-					<ProductCard
-						product={product}
-						className="bg-dark text-white"
-						key={product.id}
-						onChange={onProductCountChange}
-						value={shoppingCart[product.id]?.count || 0}
-					>
-						<ProductImage
-							className="custom-image"
-							style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }}
-						/>
-						<ProductTitle className="text-bold" />
-						<ProductButtons className="custom-buttons" />
-					</ProductCard>
-				))}
-			</div>
-
-			<div className="shopping-cart">
-				{Object.entries(shoppingCart).map(([key, product]) => (
-					<ProductCard
-						key={key}
-						product={product}
-						className="bg-dark text-white"
-						style={{ width: '100px' }}
-						onChange={onProductCountChange}
-						value={product.count}
-					>
-						<ProductImage
-							className="custom-image"
-							style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }}
-						/>
-						<ProductButtons
-							className="custom-buttons"
-							style={{
-								display: 'flex',
-								justifyContent: 'center'
-							}}
-						/>
-					</ProductCard>
-				))}
-			</div>
+			<ProductCard
+				product={product}
+				className="bg-dark text-white"
+				key={product.id}
+			>
+				<ProductImage
+					className="custom-image"
+					style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }}
+				/>
+				<ProductTitle className="text-bold" />
+				<ProductButtons className="custom-buttons" />
+			</ProductCard>
 		</div>
 	);
 };
