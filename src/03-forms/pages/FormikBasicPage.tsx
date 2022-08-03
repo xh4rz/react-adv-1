1b2e511b771d1f282460c0934da54807c621acb7
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-08-02 21:13:02 -0500

    132. Formik - Obtener información del formulario

--- src/03-forms/pages/FormikBasicPage.tsx
+++ src/03-forms/pages/FormikBasicPage.tsx
@@ -1,24 +1,48 @@
+import { useFormik } from 'formik';
 import '../styles/styles.css';
 
 export const FormikBasicPage = () => {
+	const { handleChange, values, handleSubmit } = useFormik({
+		initialValues: {
+			firstName: '',
+			lastName: '',
+			email: ''
+		},
+		onSubmit: (values) => {
+			console.log(values);
+		}
+	});
+
 	return (
 		<div>
 			<h1>Formik Basic Tutorial</h1>
 
-			<form noValidate>
+			<form onSubmit={handleSubmit} noValidate>
 				<label htmlFor="firstName">First Name</label>
-				<input type="text" name="firstName" />
-
+				<input
+					type="text"
+					name="firstName"
+					value={values.firstName}
+					onChange={handleChange}
+				/>
 				<span>First name is required</span>
 
 				<label htmlFor="firstName">Last Name</label>
-				<input type="text" name="lastName" />
-
+				<input
+					type="text"
+					name="lastName"
+					value={values.lastName}
+					onChange={handleChange}
+				/>
 				<span>Last name is required</span>
 
 				<label htmlFor="firstName">Email Address</label>
-				<input type="email" name="email" />
-
+				<input
+					type="email"
+					name="email"
+					value={values.email}
+					onChange={handleChange}
+				/>
 				<span>Email is required</span>
 				<span>Check for and valid email format</span>
 
