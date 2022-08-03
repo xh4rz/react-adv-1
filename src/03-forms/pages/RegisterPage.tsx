1ac468504cc93f3dab731b264ddaf7b271de39ed
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2022-08-02 19:42:35 -0500

    127. Formulario tradicional - con useState

--- src/03-forms/pages/RegisterPage.tsx
+++ src/03-forms/pages/RegisterPage.tsx
@@ -1,15 +1,48 @@
+import { ChangeEvent, useState } from 'react';
 import '../styles/styles.css';
 
 export const RegisterPage = () => {
+	const [registerData, setRegisterData] = useState({
+		name: 'harold',
+		email: 'xh4rz@hotmail.com',
+		password1: '123456',
+		password2: '123456'
+	});
+
+	const { name, email, password1, password2 } = registerData;
+
+	const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+		console.log(event.target.value);
+	};
+
 	return (
 		<div>
 			<h1>Register Page</h1>
 			<form>
-				<input type="text" placeholder="Name" />
-				<input type="email" placeholder="Email" />
-				<input type="password" placeholder="Password" />
-				<input type="password" placeholder="Repeat Password" />
-
+				<input
+					type="text"
+					placeholder="Name"
+					value={name}
+					onChange={onChange}
+				/>
+				<input
+					type="email"
+					placeholder="Email"
+					value={email}
+					onChange={onChange}
+				/>
+				<input
+					type="password"
+					placeholder="Password"
+					value={password1}
+					onChange={onChange}
+				/>
+				<input
+					type="password"
+					placeholder="Repeat Password"
+					value={password2}
+					onChange={onChange}
+				/>
 				<button type="submit">Create</button>
 			</form>
 		</div>
