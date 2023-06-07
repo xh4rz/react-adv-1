060424d926ae804a11925400e016348424504592
Harold Yesid Gonzalez Mancipe <xh4rz@hotmail.com>
2023-06-07 03:36:15 -0500

    152. Validaciones dinámicas adicionales

--- src/03-forms/pages/DynamicForm.tsx
+++ src/03-forms/pages/DynamicForm.tsx
@@ -18,6 +18,17 @@ for (const input of formJson) {
 		if (rule.type === 'required') {
 			schema = schema.required('Este campo es requerido');
 		}
+
+		if (rule.type === 'minLength') {
+			schema = schema.min(
+				(rule as any).value || 2,
+				`Mínimo de ${(rule as any).value || 2} caracteres`
+			);
+		}
+
+		if (rule.type === 'email') {
+			schema = schema.email('Revise el formato del email');
+		}
 	}
 
 	requiredFields[input.name] = schema;
